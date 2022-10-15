7431dc794a7c8e931f11d8dcc9daecb42c34a74c
Derine Banis <d.banis.13@gmail.com>
2022-10-14 20:38:04 -0700

    Covering edge cases

--- other-challenges/simple-css-selector-comparison/comparison.js
+++ other-challenges/simple-css-selector-comparison/comparison.js
@@ -5,18 +5,19 @@ const compare = (selectorOne, selectorTwo) => {
     let value = 0;
     tags.forEach(element => {
       if (element[0] === '.') {
-        value += (element.match(/./g).length * 10);
+    
+        value += (element.match(/./ || []).length * 10);
       }
       else if (element[0] === '#') {
         value += 100;
         if(element.includes('.')){
-          value += (element.match(/./g).length * 10);
+          value += (element.match(/./ || []).length * 10);
         }
       }
       else if(element[0] !== '*'){
         value += 1;
         if(element.includes('.')){
-          value += (element.match(/./g).length * 10);
+          value += (element.match(/./ || []).length * 10);
         }
         if(element.includes('#')){
           value += 100;
@@ -27,15 +28,15 @@ const compare = (selectorOne, selectorTwo) => {
   };
   let selectorOneValues = valueDeterminer(selectorOneTags);
   let selectorTwoValues = valueDeterminer(selectorTwoTags);
-  // console.log('Testing', selectorOne ,selectorOneValues)
-  // console.log('Testing', selectorTwo, selectorTwoValues)
+  console.log('Testing', selectorOne ,selectorOneValues)
+  console.log('Testing', selectorTwo, selectorTwoValues)
   return selectorOneValues > selectorTwoValues ? selectorOne : selectorTwo
 };
 
 
-console.log(compare("body p", "div")); // returns "body p"
-console.log(compare(".class", "#id")); // returns "#id"
-console.log(compare("div.big", ".small")); // returns "div.big"
+// console.log(compare("body p", "div")); // returns "body p"
+// console.log(compare(".class", "#id")); // returns "#id"
+// console.log(compare("div.big", ".small")); // returns "div.big"
 console.log(compare(".big", ".small")); // returns ".small" (because it appears later)
 
 console.log(compare('menu .item #checkout.active' , '#foo div#bar.red .none	'))
