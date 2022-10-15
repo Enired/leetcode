9c00a36a48cfb9d9c24825e89466276be8a04307
Derine Banis <d.banis.13@gmail.com>
2022-10-14 20:40:58 -0700

    Covering edge cases

--- other-challenges/simple-css-selector-comparison/comparison.js
+++ other-challenges/simple-css-selector-comparison/comparison.js
@@ -5,38 +5,36 @@ const compare = (selectorOne, selectorTwo) => {
     let value = 0;
     tags.forEach(element => {
       if (element[0] === '.') {
-    
-        value += (element.match(/./ || []).length * 10);
+        value += (element.match(/\./g || []).length * 10);
       }
       else if (element[0] === '#') {
         value += 100;
-        if(element.includes('.')){
-          value += (element.match(/./ || []).length * 10);
+        if (element.includes('.')) {
+          value += (element.match(/\./g || []).length * 10);
         }
       }
-      else if(element[0] !== '*'){
+      else if (element[0] !== '*') {
         value += 1;
-        if(element.includes('.')){
-          value += (element.match(/./ || []).length * 10);
+        if (element.includes('.')) {
+          value += (element.match(/\./g || []).length * 10);
         }
-        if(element.includes('#')){
+        if (element.includes('#')) {
           value += 100;
         }
       }
-    })
+    });
     return value;
   };
   let selectorOneValues = valueDeterminer(selectorOneTags);
   let selectorTwoValues = valueDeterminer(selectorTwoTags);
-  console.log('Testing', selectorOne ,selectorOneValues)
-  console.log('Testing', selectorTwo, selectorTwoValues)
-  return selectorOneValues > selectorTwoValues ? selectorOne : selectorTwo
+  // console.log('Testing', selectorOne, selectorOneValues);
+  // console.log('Testing', selectorTwo, selectorTwoValues);
+  return selectorOneValues > selectorTwoValues ? selectorOne : selectorTwo;
 };
 
-
-// console.log(compare("body p", "div")); // returns "body p"
-// console.log(compare(".class", "#id")); // returns "#id"
-// console.log(compare("div.big", ".small")); // returns "div.big"
+console.log(compare("body p", "div")); // returns "body p"
+console.log(compare(".class", "#id")); // returns "#id"
+console.log(compare("div.big", ".small")); // returns "div.big"
 console.log(compare(".big", ".small")); // returns ".small" (because it appears later)
-
-console.log(compare('menu .item #checkout.active' , '#foo div#bar.red .none	'))
+console.log(compare('menu .item #checkout.active', '#foo div#bar.red .none	'));
+console.log(compare('.item.price.active', '.active.price'));
