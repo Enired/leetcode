ccaaf72d828590dcd0126645de37500cf2d099d7
Derine Banis <d.banis.13@gmail.com>
2022-10-15 12:13:08 -0700

    Complete challenge 4. Answer verified on leetcode

--- 4/answer.js
+++ 4/answer.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
  const findMedianSortedArrays = (nums1, nums2) => {
-    
+    const allNumbers = nums1.concat(nums2).sort((a,b)=>{
+      return a-b
+    })
+    if(allNumbers.length % 2 === 0){
+      return (allNumbers[allNumbers.length / 2] + allNumbers[(allNumbers.length / 2) - 1]) / 2
+    }
+    else{
+      return allNumbers[Math.floor(allNumbers.length/2)]
+    }
 };
+
+const nums1 = [3] 
+const nums2 = [-2,-1]
+console.log(
+  findMedianSortedArrays(nums1, nums2)
+)
