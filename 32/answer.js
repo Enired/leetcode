1c311a56d42cc7765bb78e477fbf35bfbacd7adc
Derine Banis <d.banis.13@gmail.com>
2022-10-17 21:10:49 -0700

    Attempt 1. Does not pass all tests.

--- 32/answer.js
+++ 32/answer.js
@@ -3,14 +3,27 @@
  * @return {number}
  */
 const longestValidParentheses = (s) => {
-  let currentCount = 0;
+  let currentCount = []
   let highest = 0;
 
-  for(const char of s){
-    console.log(char)
+  for(let i=0; i < s.length; i++){
+    if(s[i] === '('){
+      if(s[i+1] === ')'){
+        currentCount.push(s[i])
+        currentCount.push(s[i+1])
+        highest = currentCount.length;
+      }
+    }
+    else if(s[i] === ')'){
+      if(s[i+1] !== '('){
+        currentCount = [];
+      }
+    }
   }
   return highest
 };
 
 const s1 = "(()"
-console.log(longestValidParentheses(s1));
+const s2 = ")()())"
+const s3 = ""
+console.log(longestValidParentheses(s3));
