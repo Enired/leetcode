fc700542276381356ce9efc4aa4258a7a8d61ff1
Derine Banis <d.banis.13@gmail.com>
2022-11-01 14:55:22 -0700

    Complete challenge 14

--- 14/answer.js
+++ 14/answer.js
@@ -3,5 +3,20 @@
  * @return {string}
  */
  var longestCommonPrefix = function(strs) {
+  const firstWord = strs[0]
+  let prefix = '';
+
+
+  for(let i = 0; i < firstWord.length; i++){
+    if(!strs.every(e=>e[i] === firstWord[i])){
+      return prefix
+    }
+    prefix += firstWord[i]
     
+  }
+  return prefix
 };
+const teststrs = ["dog","racecar","car"]
+const teststrs2 = ["flower","flow","flight"]
+console.log(longestCommonPrefix(teststrs))
+console.log(longestCommonPrefix(teststrs2))
